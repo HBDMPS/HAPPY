659b6237a39d9a54907dfb07e6eb76e847b7d233
HBDMPS <coolsinghabhishekpgdm@gmail.com>
2025-03-03 23:25:20 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY BIRTHDAY CUTIEEE ❤👑').split('')
+    const titles = ('HAPPY BIRTHDAY CUTIEEE ❤️👑').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
